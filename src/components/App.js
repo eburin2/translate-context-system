84671adca11ad2c8a763519e1514cadd87583209
Elliot Burin <eburin2@gmail.com>
2019-10-12 12:12:05 -0700

    setup project

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a langauge:
+          <i className="flag us"
+             onClick={() => this.onLanguageChange('english')}
+          />
+          <i className="flag nl"
+             onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
