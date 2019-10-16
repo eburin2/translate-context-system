f55c6c1bd405a2bb5b996bd23b85c7464281579f
Elliot Burin <eburin2@gmail.com>
2019-10-16 11:43:55 -0700

    add provider in App component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui button primary">Submit</button>
+    const text = this.context === 'english' ? 'submit' : 'Voorleggen';
+    return <button className="ui button primary">{text}</button>
   }
 }
 
